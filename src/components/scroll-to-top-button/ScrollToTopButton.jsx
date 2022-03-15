edaf9ef4c132e4d76367ea028e562fbc378436fc
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-15 16:49:19 +0530

    scroll to top button created

---
+++ src/components/scroll-to-top-button/ScrollToTopButton.jsx
@@ -0,0 +1,9 @@
+const ScrollToTop = () => {
+  return (
+    <a href="#" className="btn-floating btn-primary floating-up-arrow-btn">
+      <i className="fas fa-arrow-up" style={{ userSelect: "auto" }}></i>
+    </a>
+  );
+};
+
+export { ScrollToTop };
