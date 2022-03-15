7f1533510714ec39b1b319b5036c073606eb4738
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-15 16:49:33 +0530

    components imported

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { CategoryCard } from "./category-card/CategoryCard";
+export { HorizontalCard } from "./horizontal-card/HorizontalCard";
+export { Navbar } from "./navbar/Navbar";
+export { Footer } from "./footer/Footer";
+export { ScrollToTop } from "./scroll-to-top-button/ScrollToTopButton";
