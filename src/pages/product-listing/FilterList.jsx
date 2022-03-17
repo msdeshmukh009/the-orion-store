7c15726a217cd02f13b5b203fe37160ab81353ca
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-18 00:14:08 +0530

    refactor: dispatching actions from filter list

--- src/pages/product-listing/FilterList.jsx
+++ src/pages/product-listing/FilterList.jsx
@@ -1,6 +1,12 @@
 import { MultiRangeSlider } from "../../components";
+import { useCategory, useFilter, useProducts } from "../../context";
 import "./productListing.css";
 const FilterList = ({ filterStyles, setFilterStyles }) => {
+  const { state, dispatch } = useFilter();
+  const { priceRange } = useProducts();
+  const { categories } = useCategory();
+  const categoryNames = categories.map(category => category.categoryName);
+
   return (
     <section className={filterStyles} id="filters">
       <button className="filter-close-btn" onClick={() => setFilterStyles("filters")}>
@@ -11,71 +17,103 @@ const FilterList = ({ filterStyles, setFilterStyles }) => {
 
       <div className="filter-title">
         <h3>Filters</h3>
-        <button className="link-btn">Clear</button>
+        <button
+          className="link-btn"
+          onClick={() =>
+            dispatch({ type: "CLEAR", payload: { min: priceRange.min, max: priceRange.max } })
+          }
+        >
+          Clear
+        </button>
       </div>
 
       <ul className="no-style-list styled-list filter-list flex-column">
         <li className="filter-list-item">
           <h3>Price</h3>
           <div>
-            <input type="radio" name="price" id="high-to-low" />
+            <input
+              type="radio"
+              name="price"
+              id="high-to-low"
+              checked={state.sort === "high-to-low"}
+              onChange={() => dispatch({ type: "SORT", payload: "high-to-low" })}
+            />
             <label htmlFor="high-to-low">Price-high to low</label>
           </div>
           <div>
-            <input type="radio" name="price" id="low-to-high" />
+            <input
+              type="radio"
+              name="price"
+              id="low-to-high"
+              checked={state.sort === "low-to-high"}
+              onChange={() => dispatch({ type: "SORT", payload: "low-to-high" })}
+            />
             <label htmlFor="low-to-high">Price-low to high</label>
           </div>
         </li>
 
         <li>
           <h3 className="price-range-heading">Select price range</h3>
-          <MultiRangeSlider min="1000" max="10000" />
+          <MultiRangeSlider min={priceRange.min} max={priceRange.max} />
         </li>
 
         <li className="filter-list-item">
           <h3>Categories</h3>
           <div>
-            <input type="checkbox" name="category" id="coffee-mugs" />
-            <label htmlFor="coffee-mugs">Coffee Mugs</label>
+            <input
+              type="checkbox"
+              id="all"
+              checked={
+                state.category.length === 0 || state.category.length === categoryNames.length
+              }
+              onChange={() => dispatch({ type: "ADD_CATEGORY", payload: "all" })}
+            />
+            <label htmlFor="all">all</label>
           </div>
-          <div>
-            <input type="checkbox" name="category" id="posters" />
-            <label htmlFor="posters">Posters</label>
-          </div>
-          <div>
-            <input type="checkbox" name="category" id="t-shirts" />
-            <label htmlFor="t-shirts">T-shirts</label>
-          </div>
-          <div>
-            <input type="checkbox" name="category" id="accessories" />
-            <label htmlFor="accessories">Accessories</label>
-          </div>
+
+          {categoryNames.map(category => {
+            return (
+              <div key={category}>
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={state.category.includes(category)}
+                    onChange={() => dispatch({ type: "ADD_CATEGORY", payload: category })}
+                  />
+                  {category}
+                </label>
+              </div>
+            );
+          })}
         </li>
 
         <li className="filter-list-item">
           <h3>Rating</h3>
-          <div>
-            <input type="radio" name="rating" id="4-star" />
-            <label htmlFor="4-star">4 stars and above</label>
-          </div>
-          <div>
-            <input type="radio" name="rating" id="3-stars" />
-            <label htmlFor="3-stars">3 stars and above</label>
-          </div>
-          <div>
-            <input type="radio" name="rating" id="2-stars" />
-            <label htmlFor="2-stars">2 stars and above</label>
-          </div>
-          <div>
-            <input type="radio" name="rating" id="1-stars" />
-            <label htmlFor="1-stars">1 stars and above</label>
-          </div>
+          {[4, 3, 2, 1].map(rating => (
+            <div key={rating}>
+              <label>
+                <input
+                  type="radio"
+                  name="rating"
+                  checked={state.rating === rating}
+                  onChange={() => dispatch({ type: "SET_RATING", payload: rating })}
+                />
+                {rating} stars and above
+              </label>
+            </div>
+          ))}
         </li>
 
         <li>
           <h3>Other</h3>
           <div>
-            <input type="checkbox" name="include-out-of-stock" id="include-out-of-stock" />
+            <input
+              type="checkbox"
+              name="include-out-of-stock"
+              id="include-out-of-stock"
+              checked={state.includeOutOfStock}
+              onChange={() => dispatch({ type: "TOGGLE_OUT_OF_STOCK" })}
+            />
             <label htmlFor="include-out-of-stock">Include out of stock</label>
           </div>
         </li>
