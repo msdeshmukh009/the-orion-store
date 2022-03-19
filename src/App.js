2b91443a9e82803f483ce612c4fe292f9df9e4a5
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-20 01:25:24 +0530

    feat: overlay container created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import Mockman from "mockman-js";
-import { Route, Routes } from "react-router-dom";
 import { Base } from "./components";
-import { Cart, Home, ProductListing, Wishlist, Signin, Signup } from "./pages";
+import { NavigationRoutes } from "./routes/NavigationRoutes";
 
 const App = () => {
   return (
     <Base>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<ProductListing />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/mockman" element={<Mockman />} />
-      </Routes>
+      <NavigationRoutes />
     </Base>
   );
 };
