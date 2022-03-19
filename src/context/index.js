529361734473403c1434b8b911a31d9427a810ab
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-20 01:29:17 +0530

    refactor: exported useAuth & AuthProvider

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,4 @@
 export { CategoryProvider, useCategory } from "./categoryContext";
 export { ProductsProvider, useProducts } from "./productsContext";
 export { FilterProvider, useFilter } from "./filterContext";
+export { useAuth, AuthProvider } from "./authContext";
