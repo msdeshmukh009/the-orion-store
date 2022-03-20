34d6cb420878e4e4179e1409a380784d6c34eec9
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-21 03:21:22 +0530

    data: changed property names

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -11,8 +11,8 @@ export const products = [
     title: "CognitiveSurplus",
     description: "Curiosity Rover 11 oz Ceramic Mug Lorem ipsum dolor sit amet.",
     featuredProductDescription: "Astronomy Gifts, NASA Rover Mug, Engineering Gifts. ",
-    price: 1000,
-    discountedPrice: 2317,
+    discountedPrice: 1000,
+    originalPrice: 2317,
     image: "https://i.etsystatic.com/8140833/r/il/8f1cf4/2914611906/il_340x270.2914611906_t7uy.jpg",
     isFeatured: true,
     category: "Coffee Mugs",
@@ -26,8 +26,8 @@ export const products = [
     description: "Orion, Astronomy Print Of Orion Constellation In The Night Sky, .",
     featuredProductDescription:
       "Orion's Belt Star Map, Astronomy Gift, Orion Nebula, Gifts For Stargazers. ",
-    price: 2000,
-    discountedPrice: 3500,
+    discountedPrice: 2000,
+    originalPrice: 3500,
     image: "https://i.etsystatic.com/5557187/r/il/14376a/2639541341/il_340x270.2639541341_shzn.jpg",
     isFeatured: true,
     category: "Posters",
@@ -41,8 +41,8 @@ export const products = [
     description: "Orion Constellation Keychain.",
     featuredProductDescription:
       "Science Keychain - Astronomy Gift - Astronomy Keychain - Orion Keychain - Orion Nebula. ",
-    price: 300,
-    discountedPrice: 679,
+    discountedPrice: 300,
+    originalPrice: 679,
     image:
       "https://i.etsystatic.com/11861054/c/1355/1077/46/231/il/a2b863/2456472379/il_340x270.2456472379_q8ig.jpg",
     isFeatured: false,
@@ -56,8 +56,8 @@ export const products = [
     title: "CenturyPrintsStore",
     description: "Antique Astronomy Wall Art Print.",
     featuredProductDescription: "Orion Star Constellation. ",
-    price: 2000,
-    discountedPrice: 3000,
+    discountedPrice: 2000,
+    originalPrice: 3000,
     image:
       "https://i.etsystatic.com/34251122/c/800/635/95/229/il/ab98d1/3691430143/il_340x270.3691430143_mmz8.jpg",
     isFeatured: false,
@@ -71,8 +71,8 @@ export const products = [
     title: "Edkaris",
     description: "1886 Map of the Moon - Printable Moon Map ",
     featuredProductDescription: "Antique Lunar Map - Antique Map of Moon - 1800s Moon Map.",
-    price: 800,
-    discountedPrice: 1500,
+    discountedPrice: 800,
+    originalPrice: 1500,
     image:
       "https://i.etsystatic.com/15781228/c/953/758/12/346/il/b3493e/3308527660/il_340x270.3308527660_kxrl.jpg",
     isFeatured: true,
@@ -87,8 +87,8 @@ export const products = [
     description: "Orion Constellation Unisex Shirt.",
     featuredProductDescription:
       "Orion Nebula Hunter tshirt Eco Friendly Star Constellation Shirt Gift Idea Orion Belt Milky Way.",
-    price: 800,
-    discountedPrice: 1000,
+    discountedPrice: 800,
+    originalPrice: 1000,
     image:
       "https://i.etsystatic.com/13092496/r/il/e10447/3318121131/il_340x270.3318121131_oq8v.jpg",
     isFeatured: true,
@@ -103,8 +103,8 @@ export const products = [
     description: "Astronomy Gift, Astronomy Mug, Astronomy Coffee Cup, ",
     featuredProductDescription:
       "Equatorial Star Map Gift, Equatorial Star Map Mug, Equatorial Star Map Coffee Cup  ",
-    price: 1300,
-    discountedPrice: 1500,
+    discountedPrice: 1300,
+    originalPrice: 1500,
     image:
       "https://i.etsystatic.com/19006207/c/1500/1192/0/237/il/24f287/1979054507/il_340x270.1979054507_gjiu.jpg",
     isFeatured: false,
@@ -118,8 +118,8 @@ export const products = [
     title: "QualityAppearance",
     description: "Astronaut Spaceman Planets Funny Space Dwarf Solar System Astronomy T-Shirt ",
     featuredProductDescription: "Space Geeks Gifts Astronauts Lovers Birthday Party Present Moon  ",
-    price: 1100,
-    discountedPrice: 1200,
+    discountedPrice: 1100,
+    originalPrice: 1200,
     image:
       "https://i.etsystatic.com/13557012/c/1603/1274/651/20/il/e70d67/2289888423/il_340x270.2289888423_cgqa.jpg",
     isFeatured: false,
