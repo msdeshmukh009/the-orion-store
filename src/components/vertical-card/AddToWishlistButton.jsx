b7443304d1d72945dade5b1c7ee554eaf92051b8
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-22 18:52:28 +0530

    fix: added auth check to wishlist button

--- src/components/vertical-card/AddToWishlistButton.jsx
+++ src/components/vertical-card/AddToWishlistButton.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import { useWishlist } from "../../context";
+import { useAuth, useWishlist } from "../../context";
+import { useNavigate } from "react-router-dom";
 
 const AddToWishlistButton = ({ product }) => {
   const [isFetching, setIsFetching] = useState(false);
   const { addToWishlist, removeFromWishlist } = useWishlist();
+  const navigation = useNavigate();
 
   const {
     state: { wishedItems },
   } = useWishlist();
+
+  const {
+    state: { token },
+  } = useAuth();
 
   const itemExistInWishlist = wishedItems.find(item => item._id === product._id);
 
@@ -16,9 +22,11 @@ const AddToWishlistButton = ({ product }) => {
       className="card-wishlist-btn"
       disabled={isFetching ? true : false}
       onClick={() =>
-        !itemExistInWishlist
-          ? addToWishlist(product, setIsFetching)
-          : removeFromWishlist(product._id)
+        token
+          ? !itemExistInWishlist
+            ? addToWishlist(product, setIsFetching)
+            : removeFromWishlist(product._id)
+          : navigation("/signin")
       }
     >
       <i className={itemExistInWishlist ? "fas fa-heart text-danger" : "fal fa-heart"}></i>
