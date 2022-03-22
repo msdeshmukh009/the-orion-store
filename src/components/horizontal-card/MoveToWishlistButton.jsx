0cc4302ef3d562b5f64a45862e160b5df7d9c948
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-22 18:39:08 +0530

    feat: move to wishlist button for horizontal card

---
+++ src/components/horizontal-card/MoveToWishlistButton.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth, useWishlist } from "../../context";
+
+const MoveToWishlist = ({ product }) => {
+  const [isFetching, setIsFetching] = useState(false);
+  const navigation = useNavigate();
+  const {
+    state: { token },
+  } = useAuth();
+
+  const {
+    state: { wishedItems },
+    addToWishlist,
+  } = useWishlist();
+
+  const itemExistInWishlist = wishedItems.find(item => item._id === product._id);
+
+  return itemExistInWishlist ? (
+    <Link to="/wishlist" className="btn btn-outline-primary block-btn text-center">
+      Go to Wishlist
+    </Link>
+  ) : (
+    <button
+      disabled={isFetching}
+      className="btn btn-outline-primary block-btn"
+      onClick={() => (token ? addToWishlist(product, setIsFetching) : navigation("/signin"))}
+    >
+      Move to wishlist
+    </button>
+  );
+};
+
+export { MoveToWishlist };
