b852937dbd2a19ff8ca4e14729d43bda03c04df8
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-22 18:31:50 +0530

    refactor: used add to cart button component

--- src/components/wishlist-card/WishlistCard.jsx
+++ src/components/wishlist-card/WishlistCard.jsx
@@ -1,28 +1,35 @@
+import { useWishlist } from "../../context";
+import { AddToCartButton } from "../vertical-card/AddToCartButton";
+
 const WishlistCard = ({ product }) => {
-  const { image, title, description, price, discountedPrice } = product;
+  const { _id, image, title, description, originalPrice, discountedPrice } = product;
+
+  const { removeFromWishlist } = useWishlist();
+
   return (
     <div className="card vertical-card">
-      <button className="card-wishlist-btn">
+      <button className="card-close-btn" onClick={() => removeFromWishlist(_id)}>
         <i className="fas fa-times"></i>
       </button>
+
       <div className="card-image-container">
         <img className="responsive-img rounded-top-corner-img" src={image} alt="card-img" />
       </div>
+
       <div className="card-info-container">
         <span className="text-bold card-heading">{title}</span>
         <span className="card-sub-heading text-gray">{description}</span>
         <div className="price">
-          <span className="card-sub-heading text-md">₹ {price} </span>
-          <span className="text-line-through text-xs text-gray">₹ {discountedPrice}</span>
+          <span className="card-sub-heading text-md">
+            ₹ {new Intl.NumberFormat("en-IN").format(discountedPrice)}
+          </span>
+          <span className="text-line-through text-xs text-gray">
+            ₹ {new Intl.NumberFormat("en-IN").format(originalPrice)}
+          </span>
         </div>
       </div>
       <div className="card-cta-vertical">
-        <button className="btn btn-primary block-btn">
-          <span className="btn-icon">
-            <i className="fas fa-shopping-cart"></i>
-          </span>
-          Add to cart
-        </button>
+        <AddToCartButton product={product} />
       </div>
     </div>
   );
