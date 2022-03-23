7247d6639f7680688f1433622f0181bc55a5524c
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-23 20:08:44 +0530

    fix: restricting access to sigin/sigup page after user log in

--- src/routes/NavigationRoutes.jsx
+++ src/routes/NavigationRoutes.jsx
@@ -1,18 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Cart, Home, ProductListing, Wishlist, Signin, Signup } from "../pages";
 import { PrivateRoute } from "./PrivateRoute";
 import Mockman from "mockman-js";
+import { useAuth } from "../context";
 
 const NavigationRoutes = () => {
+  const {
+    state: { token },
+  } = useAuth();
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/products" element={<ProductListing />} />
       <Route path="/cart" element={<PrivateRoute element={Cart} />} />
       <Route path="/wishlist" element={<PrivateRoute element={Wishlist} />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
       <Route path="/mockman" element={<Mockman />} />
+
+      {!token && (
+        <>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </>
+      )}
+
+      <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
   );
 };
