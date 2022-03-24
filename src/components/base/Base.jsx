501a6aa113da8a50d5fc13fb19998fea1cc2ba0a
Mahesh <deshmukhmahesh009@gmail.com>
2022-03-24 21:45:25 +0530

    refactor: scroll to top on path change

--- src/components/base/Base.jsx
+++ src/components/base/Base.jsx
@@ -1,9 +1,17 @@
 import { Footer } from "../footer/Footer";
 import { Navbar } from "../navbar/Navbar";
 import { ScrollToTop } from "../scroll-to-top-button/ScrollToTopButton";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "./base.css";
 
-const Base = ({ children }) => {
+const Base = ({ children, history }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Navbar />
